78db817813bdc61d2149e5f42d1d1ea415362bad
yousef mohsen <yousefmohsen@gmail.com>
2025-12-01 14:10:53 +0200

    make the Cqrs to handle reqeust for removeLike feature

---
+++ FacebookClone.Core/Feature/Like/Command/Handlers/RemoveLikeCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using FacebookClone.Core.Feature.Like.Command.Models;
+using FacebookClone.Service.Abstract;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookClone.Core.Feature.Like.Command.Handlers
+{
+    public class RemoveLikeCommandHandler : IRequestHandler<RemoveLikeCommand, string>
+    {
+        private readonly ILikeSerivce _likeService;
+
+        public RemoveLikeCommandHandler(ILikeSerivce likeService)
+        {
+            _likeService = likeService;
+        }
+
+        public async Task<string> Handle(RemoveLikeCommand request, CancellationToken cancellationToken)
+        {
+            return await _likeService.RemoveLike(request.PostId);
+        }
+    }
+}
---
+++ FacebookClone.Core/Feature/Like/Command/Models/RemoveLikeCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookClone.Core.Feature.Like.Command.Models
+{
+    public class RemoveLikeCommand:IRequest<string>
+    {
+        public int PostId { get; set; }
+    }
+}
