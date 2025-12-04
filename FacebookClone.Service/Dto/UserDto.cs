8bc4e647caa0e35f260a488edd20f4831dfb55f4
yousef mohsen <yousefmohsen@gmail.com>
2025-12-04 13:51:24 +0200

    new feature

--- FacebookClone.Service/Dto/UserDto.cs
+++ FacebookClone.Service/Dto/UserDto.cs
@@ -11,6 +11,8 @@ namespace FacebookClone.Service.Dto
         public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        public int? PostCount { get; set; }
+        public int? FrinedCount { get; set; }
         public DateTime CreatedAt {  get; set; }
     }
 }
